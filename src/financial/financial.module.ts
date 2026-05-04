43f0c5627304a36f43a272b3a54f0cd6570d32d4
VM GPS-Marcelo <marcelo_s_almeida@hotmail.com>
2026-05-04 05:52:34 -0400

    feat: add FinancialController and FinancialService to FinancialModule

--- src/financial/financial.module.ts
+++ src/financial/financial.module.ts
@@ -1,4 +1,6 @@
 import { Module } from "@nestjs/common";
+import { FinancialController } from './financial.controller';
+import { FinancialService } from './financial.service';
 import { BanksController }      from "./banks/banks.controller";
 import { BanksService }         from "./banks/banks.service";
 import { PayableController }    from "./payable/payable.controller";
@@ -9,8 +11,8 @@ import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports:     [AuthModule],
-  controllers: [BanksController, PayableController, ReceivableController],
-  providers:   [BanksService,    PayableService,    ReceivableService],
-  exports:     [BanksService,    PayableService,    ReceivableService],
+  controllers: [FinancialController, BanksController, PayableController, ReceivableController],
+  providers:   [FinancialService, BanksService,    PayableService,    ReceivableService],
+  exports:     [FinancialService, BanksService,    PayableService,    ReceivableService],
 })
 export class FinancialModule {}
